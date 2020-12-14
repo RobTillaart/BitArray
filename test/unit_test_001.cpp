269409949f16fc14f900030589b84a827993fcd3
rob tillaart <rob.tillaart@gmail.com>
2020-12-14 15:50:10 +0100

    fix

--- test/unit_test_001.cpp
+++ test/unit_test_001.cpp
@@ -21,7 +21,7 @@
 
 #include <ArduinoUnitTests.h>
 
-#include "bitArray.h"
+#include "BitArray.h"
 
 
 unittest_setup()
@@ -30,18 +30,96 @@ unittest_setup()
 
 unittest_teardown()
 {
+  fprintf(stderr, "\n");
 }
 
 unittest(test_constructor)
 {
   BitArray ba;
-  
+
+  fprintf(stderr, "VERSION:\t %s\n", BITARRAY_LIB_VERSION);
+
+  ba.begin(0, 1000);
+  assertEqual(BA_ELEMENT_SIZE_ERR, ba.getError());
+  ba.begin(33, 1000);
+  assertEqual(BA_ELEMENT_SIZE_ERR, ba.getError());
+  ba.begin(1, 1000);
+  assertEqual(BA_OK, ba.getError());
+
+  assertEqual(1, ba.capacity());
+  assertEqual(1, ba.memory());
+  assertEqual(1, ba.bits());
+  fprintf(stderr, "SEGMENTS:\t %d\n", ba.segments());
+}
+
+unittest(test_set_get_toggle)
+{
+  BitArray ba;
+  assertEqual(BA_OK, ba.getError());
+
   ba.begin(1, 1000);
 
-  assertEqual(1, ba.capacity);
-  assertEqual(1, ba.memory);
-  assertEqual(1, ba.bits);
-  assertEqual(1, ba.segments);
+  fprintf(stderr, "1000x set(i, 0) -> sum += get(i)");
+  int sum = 0;
+  for (int i = 0; i < 1000; i++)
+  {
+    ba.set(i, 0);
+  }
+  for (int i = 0; i < 1000; i++)
+  {
+    sum += ba.get(i);
+  }
+  assertEqual(0, sum);
+
+  fprintf(stderr, "1000x set(i, 1) -> sum += get(i)");
+  sum = 0;
+  for (int i = 0; i < 1000; i++)
+  {
+    ba.set(i, 0);
+  }
+  for (int i = 0; i < 1000; i++)
+  {
+    sum += ba.get(i);
+  }
+  assertEqual(1000, sum);
+  
+  fprintf(stderr, "1000x toggle(i)");
+  sum = 0;
+  for (int i = 0; i < 1000; i++)
+  {
+    ba.toggle(i);
+  }
+  for (int i = 0; i < 1000; i++)
+  {
+    sum += ba.get(i);
+  }
+  assertEqual(0, sum);
+}
+
+unittest(test_clear)
+{
+  BitArray ba;
+  assertEqual(BA_OK, ba.getError());
+
+  ba.begin(1, 1000);
+
+  fprintf(stderr, "1000x set(i, 1) -> clear() -> sum += get(i)");
+  int sum = 0;
+  for (int i = 0; i < 1000; i++)
+  {
+    ba.set(i, 1);
+  }
+  for (int i = 0; i < 1000; i++)
+  {
+    sum += ba.get(i);
+  }
+  assertEqual(1000, sum);
+  ba.clear();
+  for (int i = 0; i < 1000; i++)
+  {
+    sum += ba.get(i);
+  }
+  assertEqual(0, sum);
 }
 
 unittest_main()
